f3e0b0fbb4b2ffb49cd59b2e4a2e6b515a3167a1
theExperienced <mizradan@gmail.com>
2019-09-17 23:53:49 +0300

    third commit

---
+++ php/sendemail.php
@@ -0,0 +1,16 @@
+
+Thanks for your message!
+
+<?php
+	
+	$userName 		= $_POST['name'];
+	$userEmail	 	= $_POST['email'];
+	$userMessage 	= $_POST['message'];
+	$to 			= "mizradan@gmail.com";
+	$subject 		= "Mail from my personal page";
+	$body 			= "Information Submitted:";
+	$body .= "\r\n Name: " . $userName;
+	$body .= "\r\n Email: " . $userEmail;
+	$body .= "\r\n Message: " . $userMessage;
+	mail($to, $subject, $body);
+?>
